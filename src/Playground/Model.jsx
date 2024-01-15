b177be2692c7ae6c1464c060d6e477f03dd3d81a
dani3l0 <daniel.jank@protonmail.com>
2024-01-15 10:50:33 +0100

    Nuke textures

--- src/Playground/Model.jsx
+++ src/Playground/Model.jsx
@@ -2,58 +2,23 @@ import { useGLTF, useTexture } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
 import { Suspense, useRef } from "react";
-import { LinearFilter, MeshBasicMaterial, NearestFilter, SRGBColorSpace, Scene } from "three";
+import { Scene } from "three";
 
 export default function Model({ store }) {
 	const obj = useGLTF("skni.glb")
 	const ref = useRef()
-	const [useHighRes,] = store.highRes
-	const [useHighQuality,] = store.highQuality
-	const { gl } = useThree()
-	const maxAnisotropy = gl.capabilities.getMaxAnisotropy()
-	gl.setPixelRatio(1 * (1 + useHighRes))
-
-	const hq = (useHighQuality) ? "-hq" : ""
-
-	const [tBuilding, tFurniture, tElectronics, tStuff] = useTexture([
-		`textures/building${hq}.jpg`,
-		`textures/furniture${hq}.jpg`,
-		`textures/electronics${hq}.jpg`,
-		`textures/stuff${hq}.jpg`,
-	])
 
 	const scene = new Scene()
 
 	for (let g of obj.scene.children) {
-		let texture;
-
-		if (g.name == "Building") texture = tBuilding
-		else if (g.name == "Furniture") texture = tFurniture
-		else if (g.name == "Electronics") texture = tElectronics
-		else if (g.name == "Stuff") texture = tStuff
-
-		if (texture) {
-			texture.flipY = false
-			texture.minFilter = NearestFilter
-			texture.magFilter = NearestFilter
-			texture.anisotropy = maxAnisotropy
-			texture.colorSpace = SRGBColorSpace
-
-			const bakedTexture = new MeshBasicMaterial({map: texture})
-			g.traverse(e => {
-				if (e.isMesh) {
-					e.material = bakedTexture
-				}
-			})
-		}
-		scene.children.push(g)
+		g.castShadow = true
+		g.receiveShadow = true
+		scene.add(g)
 	}
 
-	return <Suspense fallback={null}>
-		<RigidBody ref={ref} type="fixed" colliders="trimesh">
+	return <RigidBody ref={ref} type="fixed" colliders="trimesh">
 			<primitive object={scene}></primitive>
 		</RigidBody>
-	</Suspense>
 }
 
 useGLTF.preload("skni.glb")
--- src/Playground/Playground.jsx
+++ src/Playground/Playground.jsx
@@ -1,4 +1,4 @@
-import { KeyboardControls, PointerLockControls } from "@react-three/drei";
+import { KeyboardControls, PointerLockControls, SoftShadows, SpotLight } from "@react-three/drei";
 import { Canvas, useThree } from "@react-three/fiber";
 import { Autofocus, EffectComposer, Glitch, Noise, Pixelation, SMAA, ToneMapping } from "@react-three/postprocessing";
 import Model from "./Model";
@@ -25,40 +25,46 @@ export default function Playground({store}) {
 		]}>
 		<Canvas
 			style={{opacity: 1 - useUI / 2, transition: "opacity 1.2s"}}
-			camera={{fov: 60, far: 20}}
-			dpr={[1, 4]}
+			camera={{fov: 60, far: 20}} shadows
+			dpr={[1, 2]}
 			gl={{
 				pixelRatio: 1 * (1 + {useHighRes}),
 				outputColorSpace: SRGBColorSpace,
 				antialias: true,
-				toneMapping: ACESFilmicToneMapping
 			}}>
-			<Suspense fallback={null}>
+			<ambientLight intensity={.05} color={0xffffff}></ambientLight>
 
-				<ambientLight intensity={1.5} color={0xffffff}></ambientLight>
+			<SpotLight 
+				shadow-mapSize-height={512}
+				shadow-mapSize-width={512}
+				position={[0,3,0]}
+				distance={7}
+				angle={Math.PI /2}
+				intensity={50}
+				color={0xffbb88}
+				castShadow />
 
-				<EffectComposer>
-					{usePixelation && <Pixelation granularity={10} />}
-					{useSMAA && <SMAA />}
-					<ToneMapping
-						blendFunction={BlendFunction.COLOR}
-						adaptive={true}
-						resolution={256}
-						middleGrey={0.6}
-						maxLuminance={32}
-						averageLuminance={2}
-						adaptationRate={1.0}
-					/>
-				</EffectComposer>
+			<EffectComposer>
+				{usePixelation && <Pixelation granularity={10} />}
+				{useSMAA && <SMAA />}
+				<ToneMapping
+					blendFunction={BlendFunction.COLOR}
+					adaptive={true}
+					resolution={256}
+					middleGrey={0.6}
+					maxLuminance={32}
+					averageLuminance={2}
+					adaptationRate={1.0}
+				/>
+			</EffectComposer>
 
-				<Physics gravity={[0, -20, 0]}>
-					<Model store={store} />
-					<BaseCharacter />
-				</Physics>
+			<Physics gravity={[0, -20, 0]}>
+				<Model store={store} />
+				<BaseCharacter />
+			</Physics>
 
-				<PointerLockControls selector="#play" />
+			<PointerLockControls selector="#play" />
 
-			</Suspense>
 		</Canvas>
 	</KeyboardControls>
 }
